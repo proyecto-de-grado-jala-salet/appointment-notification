e6840be685b3617b8562b9d22803755d50e02d53
salet-gutierrez-jalafund <saletjalasofttrabajo@gmail.com>
2026-05-07 23:48:11 -0400

    feat: update variables for configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,15 +1,15 @@
 // src/config/config.ts
 export const config = {
   database: {
-    host: "shortline.proxy.rlwy.net",
-    port: 40013,
+    host: "switchyard.proxy.rlwy.net",
+    port: 51836,
     database: "railway",
     user: "postgres",
-    password: "xlBFQtBPbXEdtmrgIyueIyOFlNnZmVBI",
+    password: "IkHFPXbyXyDdmVOXQDnfAOKsqBwJXaCP",
   },
   whatsapp: {
     accessToken: "EAAUghei0XtMBPYAwKf4sOqnpRj3yRdqkPsMZBa97Ffb9YIzZAH25UaPWCNKd1ZCsbPxwnBQY8DgK9LUZAH5vdygfOGeSf4znHfTsZCEtYRKSYRPBs1kIVq7YU7zxyIOUUcdEWszzvQgl8vlzmtMHvwA0BfnPiNGYVSBUiZANMIxDvoRqoXU0xfXXBHADsBVG5LdQZDZD", // Tu token de Meta
-    phoneNumberId: "831591943365796", // Tu phone number ID
+    phoneNumberId: "1125367967319968", // Tu phone number ID
   },
   notification: {
     checkInterval: "* * * * *", // Cada minuto
